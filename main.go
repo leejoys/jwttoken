33c7b36feb1c941cb36f44d4e406aa9208731e66
leejoys <44557202+leejoys@users.noreply.github.com>
2023-05-17 16:45:30 +0300

    done

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"time"
 
@@ -14,6 +15,8 @@ type User struct {
 	IsBanned bool
 }
 
+const secretKey = "secret_key"
+
 func main() {
 
 	user := &User{
@@ -25,12 +28,16 @@ func main() {
 
 	token, err := generateToken(user)
 	if err != nil {
-		log.Println(err)
+		log.Println("generateToken error:", err)
+		return
 	}
+
+	log.Println(token)
 
 	claims, err := verifyToken(token)
 	if err != nil {
-		log.Println(err)
+		log.Println("verifyToken error:", err)
+		return
 	}
 
 	log.Println(claims)
@@ -49,9 +56,9 @@ func generateToken(user *User) (string, error) {
 	})
 
 	// Подписываем токен секретным ключом
-	tokenString, err := token.SignedString([]byte("secret_key"))
+	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
-		log.Println(1)
+		log.Println("token.SignedString error")
 		return "", err
 	}
 
@@ -60,21 +67,32 @@ func generateToken(user *User) (string, error) {
 
 // Функция для проверки токена, полученного от клиента
 func verifyToken(tokenString string) (*jwt.MapClaims, error) {
+
 	// Разбираем и проверяем токен с помощью секретного ключа
 	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return "secret_key", nil
+		return secretKey, nil
 	})
 	if err != nil {
-		log.Println("jwt.ParseWithClaims error:")
-		log.Println(err.Error())
-		return nil, err
+		if err != nil {
+			// Выводим причину невалидности токена или тип ошибки
+			if errors.Is(err, jwt.ErrTokenMalformed) {
+				log.Println("token is malformed")
+			} else if errors.Is(err, jwt.ErrTokenExpired) {
+				log.Println("token is expired")
+			} else if errors.Is(err, jwt.ErrTokenNotValidYet) {
+				log.Println("token is not valid yet")
+			} else {
+				log.Println("token is not valid")
+			}
+			return nil, err
+		}
 	}
 
 	// Извлекаем claims из токена
 	claims, ok := token.Claims.(*jwt.MapClaims)
 	if !ok || !token.Valid {
-		log.Println("token.Claims.(*jwt.MapClaims): !ok || !token.Valid")
-		return nil, err
+		log.Println("token claims are not valid")
+		return nil, errors.New("invalid token")
 	}
 
 	return claims, nil
